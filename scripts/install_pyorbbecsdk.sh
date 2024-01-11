a1e9102ef942019e01db3aa8370cf15aa7d44a8d
Sherry <sherrylin42@gmail.com>
2024-01-11 16:00:31 -0500

    fix: bugs in cli

--- scripts/install_pyorbbecsdk.sh
+++ scripts/install_pyorbbecsdk.sh
@@ -7,6 +7,3 @@ cd build
 cmake -Dpybind11_DIR=`pybind11-config --cmakedir` ..
 make -j4
 make install
-
-# add python path to .bashrc
-echo "export PYTHONPATH=$PYTHONPATH:$(pwd)/install/lib/" >> ~/.bashrc
