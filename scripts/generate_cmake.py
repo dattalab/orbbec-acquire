cb0f0d0fd5686e78ff4fdfc12155ec8b883d0f1f
Sherry <sherrylin42@gmail.com>
2024-01-11 14:14:57 -0500

    chore: fix the path

--- scripts/generate_cmake.py
+++ scripts/generate_cmake.py
@@ -9,12 +9,14 @@ pybind_path = os.path.join(os.path.dirname(pybind11.__file__), "share/cmake/pybi
 # construct the string to add to the CMakeLists.txt
 wstring = f'set(Python3_ROOT_DIR "{python_path}")\n'+ f'set(pybind11_DIR "{pybind_path}")\n'
 
-with open('./CMakeLists_template.txt', 'r') as f:
+base_dir = os.getcwd()
+
+with open(os.path.join(base_dir, 'scripts/CMakeLists_template.txt'), 'r') as f:
     contents = f.readlines()
 
 # insert the string at line 31 right before find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
 contents.insert(31, wstring)
 
-with open('./CMakeLists.txt', 'w') as f:
+with open(os.path.join(base_dir, 'scripts/CMakeLists.txt'), 'w') as f:
     contents = "".join(contents)
     f.write(contents)
