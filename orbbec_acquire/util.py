7d86b53fd9b434fec5796525140c990cba5a3074
Sherry <sherrylin42@gmail.com>
2024-01-28 19:41:11 -0500

    chore: revert changes in util.py

--- orbbec_acquire/util.py
+++ orbbec_acquire/util.py
@@ -11,27 +11,22 @@ from multiprocessing import Process, Queue
 from pyorbbecsdk import *
 
 
-def display_images(display_queue: Queue):
+def display_images(display_queue):
     """
     display captured images
 
     Args:
         display_queue (multiprocessing.queues.Queque): the data stream from the camera to be displayed
     """
-    # image_dtype = cv2.CV_16UC1
-    # data_type = np.uint16
-    max_data = 5500
     while True:
         data = display_queue.get()
-
         if len(data) == 0:
             cv2.destroyAllWindows()
             break
         else:
             ir = data[0]
-            # cv2.normalize(ir, ir, 0, max_data, cv2.NORM_MINMAX, dtype=image_dtype)
-            ir = np.clip(ir + 100, 160, max_data)
-            ir = np.clip(((np.log(ir) - 5) * 70), 0, None).astype(np.uint8)
+            ir = np.clip(ir + 100, 160, 5500)
+            ir = ((np.log(ir) - 5) * 70).astype(np.uint8)
 
             cv2.imshow("ir", ir)
             if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -40,7 +35,7 @@ def display_images(display_queue: Queue):
         while True:
             try:
                 display_queue.get_nowait()
-            except Exception:
+            except:
                 break
 
 
@@ -183,6 +178,7 @@ def write_metadata(
     subject_name,
     session_name,
     depth_resolution=[640, 576],
+    little_endian=True,
     color_resolution=[640, 576],
 ):
     """
@@ -193,6 +189,7 @@ def write_metadata(
         subject_name (str): subject name of the recording
         session_name (str): session name of the recording
         depth_resolution (list, optional): frame resolution of depth videos. Defaults to [640, 576].
+        little_endian (bool, optional): boolean flag that indicates if depth data is little endian. Defaults to True.
         color_resolution (list, optional): frame resolution of ir video. Defaults to [640, 576].
     """
 
@@ -201,6 +198,7 @@ def write_metadata(
         "SubjectName": subject_name,
         "SessionName": session_name,
         "DepthResolution": depth_resolution,
+        "IsLittleEndian": little_endian,
         "ColorResolution": color_resolution,
         "StartTime": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
     }
@@ -231,11 +229,13 @@ def start_recording(
         device_id (int, optional): camera id number if there are multiple cameras. Defaults to 0.
     """
 
-    PRINT_INTERVAL = 30
-    # MIN_DEPTH = 20  # 20mm
-    # MAX_DEPTH = 10000
+    PRINT_INTERVAL = 15
+    MIN_DEPTH = 20  # 20mm
+    MAX_DEPTH = 10000
 
-    filename_prefix = os.path.join(base_dir, "session_" + datetime.now().strftime("%Y%m%d%H%M%S"))
+    filename_prefix = os.path.join(
+        base_dir, "session_" + datetime.now().strftime("%Y%m%d%H%M%S")
+    )
     os.makedirs(filename_prefix, exist_ok=True)
 
     # write recording metadata
@@ -244,7 +244,9 @@ def start_recording(
     # start image writing process
     image_queue = Queue()
     write_process = Process(
-        target=write_images, args=(image_queue, filename_prefix), kwargs={"save_ir": save_ir}
+        target=write_images,
+        args=(image_queue, filename_prefix),
+        kwargs={"save_ir": save_ir},
     )
     write_process.start()
 
@@ -274,15 +276,15 @@ def start_recording(
     system_timestamps = []
     device_timestamps = []
     start_time = time.time()
-    print("Start time:", start_time)
+    print(start_time)
     count = 0
 
     # the actual recording
     try:
         while time.time() - start_time < recording_length:
-            frames = pipeline.wait_for_frames(100)
+            frames = pipeline.wait_for_frames(1000)
             if frames is None:
-                # print("Dropped frame")
+                print("Dropped frame")
                 continue
 
             # record system timestamp
@@ -300,10 +302,11 @@ def start_recording(
 
             depth_data = np.frombuffer(depth_frame.get_data(), dtype=np.uint16)
             depth_data = depth_data.reshape((height, width))
-            depth_data = (depth_data * scale).astype(np.uint16)
-            # depth_data = np.where(
-            #     (depth_data > MIN_DEPTH) & (depth_data < MAX_DEPTH), depth_data, 0
-            # )
+            depth_data = depth_data.astype(np.float32) * scale
+            depth_data = np.where(
+                (depth_data > MIN_DEPTH) & (depth_data < MAX_DEPTH), depth_data, 0
+            )
+            depth_data = depth_data.astype(np.uint16)
 
             # get ir frames
             ir_frame = frames.get_ir_frame()
@@ -316,21 +319,36 @@ def start_recording(
             ir_data = np.resize(ir_data, (height, width, 1))
 
             # info about ir for rendering
-            # data_type = np.uint16
-            # image_dtype = cv2.CV_16UC1
-            # max_data = 65535
-            # cv2.normalize(ir_data, ir_data, 0, max_data, cv2.NORM_MINMAX, dtype=image_dtype)
-            # ir_data = ir_data.astype(data_type)
+            data_type = np.uint16
+            image_dtype = cv2.CV_16UC1
+            max_data = 65535
+            cv2.normalize(
+                ir_data, ir_data, 0, max_data, cv2.NORM_MINMAX, dtype=image_dtype
+            )
+            ir_data = ir_data.astype(data_type)
 
             image_queue.put((ir_data, depth_data))
             if display_frames and count % 2 == 0:
                 display_queue.put((ir_data,))
 
             if count > 0:
-                if display_time and (count % PRINT_INTERVAL) == 0:
-                    fps = len(system_timestamps) / (max(system_timestamps) - min(system_timestamps))
-                    print(f"\rRecorded {int(time.time() - start_time):02d} of {recording_length} seconds", end="")
-                    print(f" - Current frame rate {fps:0.2f} fps", end="")
+                if display_time and count % PRINT_INTERVAL:
+                    sys.stdout.write(
+                        "\rRecorded "
+                        + repr(int(time.time() - start_time))
+                        + " out of "
+                        + repr(recording_length)
+                        + " seconds "
+                        + "- Current Frame rate "
+                        + str(
+                            round(
+                                len(system_timestamps)
+                                / (max(system_timestamps) - min(system_timestamps)),
+                                2,
+                            )
+                        )
+                        + " fps"
+                    )
             count += 1
     except OSError:
         print("Recording stopped early")
@@ -339,9 +357,21 @@ def start_recording(
         pipeline.stop()
         device_timestamps = np.array(device_timestamps)
 
-        np.savetxt(os.path.join(filename_prefix, "depth_ts.txt"), device_timestamps, fmt="%f")
-        fps = len(system_timestamps) / (max(system_timestamps) - min(system_timestamps))
-        print(f" - Session average frame rate = {fps:0.2f} fps")
+        np.savetxt(
+            os.path.join(filename_prefix, "depth_ts.txt"), device_timestamps, fmt="%f"
+        )
+        print(
+            " - Session Average Frame rate = ",
+            str(
+                round(
+                    len(system_timestamps)
+                    / (max(system_timestamps) - min(system_timestamps)),
+                    2,
+                )
+            )
+            + " fps",
+        )
+
         image_queue.put(tuple())
         write_process.join()
 
